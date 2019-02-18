9dfd9424b3cf3d2731325dfe1d2b725f52651fdb
austin reynolds <elektricwebdesign@gmail.com>
2019-02-18 13:49:51 -0700

    PACKAGES ADDED: "express": "^4.16.4", "webpack-dev-middleware": "^3.5.2", "webpack-dev-server": "^3.1.14", "webpack-hot-middleware": "^2.24.3" =================================== Added minimal express server added webpack HMR to express via middleware

---
+++ src/server/express.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 8080;
+const webpack = require("webpack");
+const config = require("../../config/webpack.dev.js");
+const compiler = webpack(config);
+const webpackDevMiddleware = require('webpack-dev-middleware')(
+  compiler,
+  config.devServer
+);
+const webpackHotMiddleware = require("webpack-hot-middleware")(compiler);
+app.use(webpackDevMiddleware);
+app.use(webpackHotMiddleware);
+const staticMiddleware = express.static('dist');
+app.use(staticMiddleware);
+app.listen(port, () => {
+  console.log('server is listening on ', port);
+})
